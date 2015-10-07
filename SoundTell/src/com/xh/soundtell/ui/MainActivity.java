6e9c922cb3a4ea837f6afa20b51c85d94b11b6ee
Administrator <Administrator@Admin-PC>
2015-10-08 00:57:25 +0800

    唱歌的popupWindow。唱歌Activity 和 歌曲类型Activity

--- SoundTell/src/com/xh/soundtell/ui/MainActivity.java
+++ SoundTell/src/com/xh/soundtell/ui/MainActivity.java
@@ -1,20 +1,31 @@
 package com.xh.soundtell.ui;
 
+import android.content.Intent;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.xh.soundtell.R;
+import com.xh.soundtell.SingRecordActivity;
+import com.xh.soundtell.SingStyleActivity;
 import com.xh.soundtell.setting.SettingHelper;
 import com.xh.soundtell.ui.fragment.ActivityFragment;
 import com.xh.soundtell.ui.fragment.FriendsFragment;
 import com.xh.soundtell.ui.fragment.HotFragment;
 import com.xh.soundtell.ui.fragment.MyFragment;
+import com.xh.soundtell.util.DensityUtil;
 
 public class MainActivity extends FragmentActivity implements OnClickListener {
 
@@ -22,6 +33,8 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 
 	private TextView tv1, tv2, tv3, tv4;
 	private ImageView iv1;
+
+	private PopupWindow popupWindow;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +66,7 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 		if (currentView != null && currentView.equals(v)) {
 			return;
 		}
+		Intent intent=null;
 		FragmentManager fm = getSupportFragmentManager();
 		FragmentTransaction ft = fm.beginTransaction();
 		switch (v.getId()) {
@@ -60,28 +74,52 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 			MyFragment myFragment = new MyFragment();
 			ft.replace(R.id.frame_layout, myFragment);
 			ft.commit();
+			setButton(v);
 			break;
 		case R.id.tv2:
 			HotFragment hotFragment = new HotFragment();
 			ft.replace(R.id.frame_layout, hotFragment);
 			ft.commit();
+			setButton(v);
 			break;
 		case R.id.tv3:
 			ActivityFragment activityFragment = new ActivityFragment();
 			ft.replace(R.id.frame_layout, activityFragment);
 			ft.commit();
+			setButton(v);
 			break;
 		case R.id.tv4:
 			FriendsFragment friendsFragment = new FriendsFragment();
 			ft.replace(R.id.frame_layout, friendsFragment);
 			ft.commit();
+			setButton(v);
 			break;
-		default:
+		case R.id.iv1:
+			//show popupwindow
+			getPopwindow();
 			break;
+			
+		/******* popupWindow ******/	
+		case R.id.popupsing_ll1:
+			intent=new Intent(this,SingRecordActivity.class);
+			showDisPopWindow();
+			break;	
+		case R.id.popupsing_ll2:
+			intent=new Intent(this,SingRecordActivity.class);
+			showDisPopWindow();
+			break;	
+		case R.id.popupsing_ll3:
+			intent=new Intent(this,SingStyleActivity.class);
+			showDisPopWindow();
+			break;	
+		case R.id.popupsing_back:
+			showDisPopWindow();
+			break;	
 		}
-		setButton(v);
+		if(intent!=null){
+			startActivity(intent);
+		}
 	}
-
 	private void setButton(View v) {
 		if (currentView != null && currentView != v) {
 			currentView.setSelected(false);
@@ -89,5 +127,53 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 		v.setSelected(true);
 		currentView = v;
 	}
+	private void getPopwindow() {
+     	final View	popView = LayoutInflater.from(this).inflate(R.layout.popup_sing,
+				null);
+		View llView1=popView .findViewById(R.id.popupsing_ll1);
+		View llView2=popView .findViewById(R.id.popupsing_ll2);
+		View llView3=popView .findViewById(R.id.popupsing_ll3);
+		View backView=popView .findViewById(R.id.popupsing_back);
+		
+		llView1.setOnClickListener(this);
+		llView2.setOnClickListener(this);
+		llView3.setOnClickListener(this);
+		backView.setOnClickListener(this);
+		
+     	
+      popupWindow = new PopupWindow(popView,DensityUtil.getScreenWidthAndHeight(this)[0],
+     			DensityUtil.getScreenWidthAndHeight(this)[1]-DensityUtil.dip2px(this, 50));
+		popupWindow.setFocusable(true);
+		popupWindow.setOutsideTouchable(true);
+		popupWindow.setBackgroundDrawable(new BitmapDrawable());
+		popupWindow
+				.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
+		//		popView.setOnTouchListener(new OnTouchListener() {
+//			public boolean onTouch(View v, MotionEvent event) {
+//				int height = popView.findViewById(R.id.pop_layout).getTop();
+//				int y = (int) event.getY();
+//				if (event.getAction() == MotionEvent.ACTION_UP) {
+//					if (y < height) {
+//						popupWindow.dismiss();
+//					}
+//				}
+//				return true;
+//			}
+//		});
+		showDisPopWindow();
+	}
+	
+	public void showDisPopWindow() {
+		if (popupWindow != null) {
+			if (popupWindow.isShowing()) {
+				popupWindow.dismiss();
+			} else {
+				popupWindow.showAtLocation(getWindow().getDecorView(),
+						Gravity.TOP, 0, DensityUtil.dip2px(this, 50));
+			}
+		}
+	}
+	
+	
 }
--- SoundTell/src/com/xh/soundtell/ui/fragment/HotFragment.java
+++ SoundTell/src/com/xh/soundtell/ui/fragment/HotFragment.java
@@ -1,7 +1,25 @@
 package com.xh.soundtell.ui.fragment;
 
+import android.app.Activity;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class HotFragment extends Fragment {
-
+	
+	Activity act;
+	public HotFragment(){}
+	
+	 @Override
+	public void onAttach(Activity activity) {
+     	act=activity;
+		super.onAttach(activity);
+	}
+  @Override
+public View onCreateView(LayoutInflater inflater, ViewGroup container,
+		Bundle savedInstanceState) {
+	return super.onCreateView(inflater, container, savedInstanceState);
+}
 }
